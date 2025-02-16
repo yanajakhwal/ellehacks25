adbb600d69cfef5623bf70c87a147d403aeb650b
Saryu <saryurawaley@icloud.com>
2025-02-16 00:57:51 -0500

    c

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,34 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 
+#twilio API integration - from site
+import os
+from twilio.rest import Client
+
+#twilio API key and token - from site
+account_sid = 'AC24c974847f9bdcd9247c88ddf64df675'
+auth_token = '5cda0eb1b7a3348d82935d941f3f0e32'
+client = Client(account_sid, auth_token)
+
+
+#make this based off of the user input - when they click login button
+verification = client.verify \
+    .v2 \
+    .services('VAea5cf22f2a59e8d369b782f3557f8789') \
+    .verifications \
+    .create(to='+16476796931', channel='sms')
+print(verification.sid)
+verification_check = client.verify.v2.services(
+    "VAea5cf22f2a59e8d369b782f3557f8789"
+).verification_checks.create(to="+16476796931", code="123456")
+print(verification_check.status)
+
+#verifiying user input codes 
+#verification_check = client.verify.services('VAea5cf22f2a59e8d369b782f3557f8789').verification_checks.create(to='+16476796931', code='123456')
+#print(verification_check.status)
+
+
+
 app = FastAPI()
 
 # Allow requests from any origin (for development)
@@ -35,7 +63,7 @@ async def receive_geofence_status(status: GeofenceStatus):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8082)
 
 class GeofenceStatus(BaseModel):
     inRange: bool
@@ -55,6 +83,19 @@ async def receive_geofence_status(status: GeofenceStatus):
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+
+# range - this based off boolean variable name = isInRange 
+if isInRange == False:
+    message = client.messages.create(
+       body="Patient is out of range",
+       from_="+12893019431",
+        to="+16476796931",
+    )
+print(message.body)
+
+
 
 
 
